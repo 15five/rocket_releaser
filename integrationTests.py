598526c52fd27afc7576bc60551337cc7c971e2f
caleb15 <caleb@15five.com>
2020-10-08 17:16:26 -0700

    fix order of arguments in integration test

--- integrationTests.py
+++ integrationTests.py
@@ -9,14 +9,15 @@ from rocket_releaser import release_notes
 
 slack_text = release_notes.release_notes(
     "github key",
-    "jira key",
-    "674913b6cf8f49308e61c90c6f110588d521990c",
-    "950a00d8465ec257cb0ffac56e601ae6bf9a6106",
+    "82a86d4",
+    "9253704",
     "slack key",
     "staging",
     "staging",
-    "/home/caleb/Documents/fifteen5",
-    "staging",
+    "",
+    "master",
+    "15five",
+    "rocket_releaser",
 )
 
 
