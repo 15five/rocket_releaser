62ae20fa3d90174410932fb4ca434c4294c4c47e
caleb15 <caleb@15five.com>
2021-08-11 10:22:41 -0700

    MOAR TESTS

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -16,6 +16,21 @@ def test_changelog_note_should_be_in_right_format():
     )
 
 
+def test_changelog_note_picks_up_right_label():
+    pull_request_dict = {
+        "number": 1,
+        "body": "RELEASES\nDESCRIPTION_1",
+        "labels": ["ignore", "feat-CATEGORY1"],
+    }
+    assert (
+        ChangeLog([pull_request_dict], "org_name", "repo_name").parse_bodies()
+        == """*CATEGORY1*
+• DESCRIPTION_1 <https://github.com/org_name/repo_name/pull/1|PR-1>
+
+"""
+    )
+
+
 def test_changelog_note_uncategorized():
     pull_request_dict = {
         "number": 1,
@@ -26,6 +41,25 @@ def test_changelog_note_uncategorized():
         ChangeLog([pull_request_dict], "org_name", "repo_name").parse_bodies()
         == """*Uncategorized*
 • DESCRIPTION_1 <https://github.com/org_name/repo_name/pull/1|PR-1>
+
+"""
+    )
+
+
+def test_changelog_note_multiple_lines():
+    pull_request_dict = {
+        "number": 1,
+        "body": "RELEASES\nSentence1. Sentence 2.\nSentence 3.",
+        "labels": [],
+    }
+
+    # I'm not married to this particular format.
+    # It may be better to keep newlines instead of transforming them into spaces.
+    # After all, the bullet points distinugish between different PR's.
+    assert (
+        ChangeLog([pull_request_dict], "org_name", "repo_name").parse_bodies()
+        == """*Uncategorized*
+• Sentence1. Sentence 2. Sentence 3. <https://github.com/org_name/repo_name/pull/1|PR-1>
 
 """
     )
